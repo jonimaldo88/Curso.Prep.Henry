fc335247c0d56e0b471965a558c170872c0eae3c
jonimaldo88 <maldonado_jonatan@hotmail.com>
2022-05-02 21:46:45 -0300

    recordando
    recordando

---
+++ cuenta.js
@@ -0,0 +1,26 @@
+var combos = [1,
+    2,
+    1,
+    2,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    2,
+    1,
+    2,
+    1,
+    2];
+var contenedor = [];
+
+ for ( i=0, combos % 2===0, i++) {
+    return contenedor;
+  }  false;
+
